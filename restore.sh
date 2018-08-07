b4bab31b53858dbe1599455164fbb7d500030c7e
Nicolas North <gi@nk.ai>
2018-08-07 22:28:09 +0200

    Improve formatting and 'set_option's

--- restore.sh
+++ restore.sh
@@ -16,7 +16,7 @@ configdir=$installdir/config # Don't change! | No trailing slash!
 
 echo
 
-echo "${b}Initiating restore script...${x}"
+echo "${b-}Initiating restore script...${x-}"
 echo
 
 
@@ -38,13 +38,13 @@ then
 
     if [ ! $sshconnectionstatus = 0 ]
     then
-        echo "${r}${b}You appear to be connected via SSH on port 443 thanks to SSLH.${x}"
+        echo "${r-}${b-}You appear to be connected via SSH on port 443 thanks to SSLH.${x-}"
         echo
-        echo "${b}This is usually perfectly fine, but not during installation, as we will be terminating SSLH during one of the steps.${x}"
+        echo "${b-}This is usually perfectly fine, but not during installation, as we will be terminating SSLH during one of the steps.${x-}"
         echo
-        echo "${b}Please connect via your normal SSH port until you're finished re-installing aenigma${x}"
+        echo "${b-}Please connect via your normal SSH port until you're finished re-installing aenigma${x-}"
         echo
-        echo "${b}Exiting...${x}"
+        echo "${b-}Exiting...${x-}"
         echo
         exit
     fi
@@ -55,11 +55,11 @@ fi
 
 if [[ $EUID -ne 0 ]]
 then
-    echo "${r}${b}This script must be run as root. Run it as:${x}"
+    echo "${r-}${b-}This script must be run as root. Run it as:${x-}"
     echo
     echo "sudo bash aenigma/install.sh"
     echo
-    echo "${b}Exiting...${x}"
+    echo "${b-}Exiting...${x-}"
     echo
     exit
 fi
@@ -68,7 +68,7 @@ fi
 
 if [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/16.04/' `" != "Ubuntu 16.04 LTS" ]
 then
-    echo "${r}${b}aenigma only runs on Ubuntu 16.04. You are running:${x}"
+    echo "${r-}${b-}aenigma only runs on Ubuntu 16.04. You are running:${x-}"
     echo
     lsb_release -d | sed 's/.*:\s*//'
     echo
@@ -79,18 +79,18 @@ fi
 
 if [ -f $basedir/DFBS/run-ok ]
 then
-    echo "${g}${b}Debian First Boot Setup was previously run successfully. Continuing...${x}"
+    echo "${g-}${b-}Debian First Boot Setup was previously run successfully. Continuing...${x-}"
     echo
 else
-    echo "${r}${b}Debian First Boot Setup was NOT previously run successfully OR the system was not rebooted at the end.${x}"
+    echo "${r-}${b-}Debian First Boot Setup was NOT previously run successfully OR the system was not rebooted at the end.${x-}"
     echo
-    read -p "${b}Run it now? (Y/n): ${x}" -n 1 -r
+    read -p "${b-}Run it now? (Y/n): ${x-}" -n 1 -r
     echo
     if [[ ! $REPLY =~ ^[Nn]$ ]]
     then
-        echo "${b}Ok, running DFBS now...${x}"
+        echo "${b-}Ok, running DFBS now...${x-}"
         echo
-        echo "${b}After you're done running DFBS [make sure you reboot this machine at the end], simply run the aenigma installer again.${x}"
+        echo "${b-}After you're done running DFBS [make sure you reboot this machine at the end], simply run the aenigma installer again.${x-}"
         echo
         sleep 3
         if [ -d "/root/Debian-First-Boot-Setup" ]
@@ -102,13 +102,13 @@ else
         bash Debian-First-Boot-Setup/script.sh
         exit
     else
-        echo "${b}No problem, you can run it [make sure you reboot this machine at the end] by executing the following commands:${x}"
+        echo "${b-}No problem, you can run it [make sure you reboot this machine at the end] by executing the following commands:${x-}"
         echo
-        echo "${b} | git clone https://github.com/openspace42/Debian-First-Boot-Setup |${x}"
+        echo "${b-} | git clone https://github.com/openspace42/Debian-First-Boot-Setup |${x-}"
         echo
-        echo "${b} | bash Debian-First-Boot-Setup/script.sh |${x}"
+        echo "${b-} | bash Debian-First-Boot-Setup/script.sh |${x-}"
         echo
-        echo "${b}Exiting...${x}"
+        echo "${b-}Exiting...${x-}"
         echo
         exit
     fi
@@ -116,7 +116,7 @@ fi
 
 
 
-echo "${b}Now installing dependencies...${x}"
+echo "${b-}Now installing dependencies...${x-}"
 echo
 hostname="$(cat /etc/hostname)"
 apt-add-repository ppa:duplicity-team/ppa -y
@@ -129,19 +129,19 @@ debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Si
 apt-get install -y mailutils
 /etc/init.d/postfix reload
 echo
-echo "${b}Finished installing dependencies.${x}"
+echo "${b-}Finished installing dependencies.${x-}"
 echo
 
 
 
-echo "${g}${b}Preflight check complete.${x}"
+echo "${g-}${b-}Preflight check complete.${x-}"
 echo
-echo "${b}Now proceeding with aenigma restore...${x}"
+echo "${b-}Now proceeding with aenigma restore...${x-}"
 echo
 
 sudo bash aenigma/tools/aenigma-restore
 
-echo "${b}Exiting restore script.${x}"
+echo "${b-}Exiting restore script.${x-}"
 echo
 
 
